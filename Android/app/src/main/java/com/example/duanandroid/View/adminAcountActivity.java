395b8e574ecaeee815c75401b764a8f1e1da6159
Thao2508 <thithao25082004@gmail.com>
2024-10-03 11:01:42 +0700

    layout adminAcount and changePass

---
+++ Android/app/src/main/java/com/example/duanandroid/View/adminAcountActivity.java
@@ -0,0 +1,29 @@
+package com.example.duanandroid.View;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.duanandroid.R;
+import com.example.duanandroid.databinding.ActivityAdminAcountBinding;
+
+public class adminAcountActivity extends AppCompatActivity {
+
+    private ActivityAdminAcountBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_admin_acount);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
