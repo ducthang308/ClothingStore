184d8b227e8d92c5caa3448c02fcf37c9e2a10fc
NguyenVanThang <nguyenvanthang30102004@gmail.com>
2024-11-07 17:35:59 +0700

    fix bug after merge

--- Android/app/src/main/java/com/example/duanandroid/View/AdminChitietSpActivity.java
+++ Android/app/src/main/java/com/example/duanandroid/View/AdminChitietSpActivity.java
@@ -49,17 +49,7 @@ public class AdminChitietSpActivity extends AppCompatActivity {
         reviewRecyclerView = findViewById(R.id.review_list);
         reviewRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Tạo danh sách đánh giá (ví dụ tạm thời)
-        reviewList = new ArrayList<>();
-        reviewList.add(new Review("imageUrl", "Great product!", 4.5f, 0));
-        reviewList.add(new Review("imageUrl", "Very satisfied!", 5.0f, 0));
-        reviewList.add(new Review("imageUrl", "Good quality.", 4.0f, 0));
-
-        // Tạo danh sách người dùng (ví dụ tạm thời)
-        userList = new ArrayList<>();
-        userList.add(new User(1, "John Doe", "Address", "0123456789", "john@example.com", 1, null, "john", "password", "active", 0, 0));
-        userList.add(new User(2, "Jane Smith", "Address", "0123456789", "jane@example.com", 1, null, "jane", "password", "active", 0, 0));
-        userList.add(new User(3, "Alice Brown", "Address", "0123456789", "alice@example.com", 1, null, "alice", "password", "active", 0, 0));
+    
 
         // Thiết lập Adapter cho RecyclerView
         reviewAdapter = new ReviewAdapter(reviewList, userList);
